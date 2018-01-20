35dbc97634eafe30f17a4774b0a20dce424352d3
Husain Raja <Husain Raja@192.168.0.7>
2018-01-20 11:26:51 +0530

    First Commit

---
+++ resume/accepting.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+var n=prompt("enter you name","");
+document.getElementById("name").innerHTML=n;
+var email=prompt("enter you mail id","");
+document.getElementById("email").innerHTML=email;
+var num=prompt("enter you number","");
+document.getElementById("number").innerHTML=num;
